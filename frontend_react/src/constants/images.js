a5e6c09517eea0ae54a806b67fff7b1a6a4e2715
Julian Currie <jccurrie@uab.edu>
2023-04-20 23:09:22 -0500

    adding images, css styling, various changes

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,5 +1,21 @@
-import logo from '../assets/Julian_Logo_Mask.png';
+import logo from '../assets/logo.svg';
+import circle from '../assets/circle.svg';
+import profile from '../assets/profile.png';
+import background from '../assets/Julian_Logo_Mask.png';
+import css from '../assets/css.png'
+import figma from '../assets/figma.png'
+import html from '../assets/html.png'
+import javascript from '../assets/javascript.png'
+import git from '../assets/git.png'
 
 export default {
-    logo
+    logo,
+    circle,
+    profile,
+    background,
+    css,
+    figma,
+    html,
+    javascript,
+    git
 }
